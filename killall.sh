7c772a547d7d71cd5a9af829847263decc8553ea
Feng Ren <alogfans@gmail.com>
2023-11-10 10:01:44 +0800

    Add descriptions to interprete results

---
+++ killall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ae/collect/common.sh
+if [ $HOSTNAME != $client_machine ]
+then
+    echo "You must run this script in $client_machine"
+    echo "Hint: you may check out the field 'client_machine' in ae/collect/common.sh"
+    exit 1
+fi
+
+cd build
+bash ../ae/collect/killall.sh
